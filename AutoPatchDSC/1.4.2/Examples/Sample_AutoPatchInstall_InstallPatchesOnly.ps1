3388335105e1a9aa072e284b72b8bb2f6e6f157c
Chris Madole <chmadole@microsoft.com>
2017-04-20 09:04:06 -0700

    Added Microsoft Open Source Code of Conduct disclaimer.

---
+++ AutoPatchDSC/1.4.2/Examples/Sample_AutoPatchInstall_InstallPatchesOnly.ps1
@@ -0,0 +1,18 @@
+# This example installs any patches avialable from WSUS between 2am and 4am.
+# It does not execute any reboots.
+Configuration OnlyPatchServerNoReboot {
+    If (-not (Get-Module PSWindowsUpdate)) {Import-Module PSWindowsUpdate}
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName @{ModuleName="AutoPatchDsc"; RequiredVersion="1.4.2"}
+
+    Node 'Server01'{
+        #Install patches during maintenance window
+        AutoPatchInstall InstallOSPatches {
+            Name                          = 'InstallOSPatches'
+            PreflightWindowStart          = 'January 1st, 2017 1am'
+            PatchWindowStart              = 'January 1st, 2017 2am'
+            PatchWindowEnd                = 'January 1st, 2017 4am'
+            InstallPatchesDuringPreflight = $false
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AutoPatchDSC
 PowerShell Desired State Configuration Resource to patch Windows targets with a defined maintenance window.
+
+This project has adopted the Microsoft Open Source Code of Conduct.
+For more information see the Code of Conduct FAQ or contact opencode@microsoft.com with any additional questions or comments.
