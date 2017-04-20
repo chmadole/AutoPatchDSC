bd47ff459dde66a8de901170c086f218bd824539
Chris Madole <chmadole@microsoft.com>
2017-04-20 09:11:38 -0700

    added logfile parameter to example

--- AutoPatchDSC/1.4.2/Examples/Sample_AutoPatchInstall_InstallPatchesOnly.ps1
+++ AutoPatchDSC/1.4.2/Examples/Sample_AutoPatchInstall_InstallPatchesOnly.ps1
@@ -1,9 +1,11 @@
 # This example installs any patches avialable from WSUS between 2am and 4am.
 # It does not execute any reboots, nor install patches during the preflight window.
 Configuration OnlyPatchServerNoReboot {
-    If (-not (Get-Module PSWindowsUpdate)) {Import-Module PSWindowsUpdate}
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     Import-DscResource -ModuleName @{ModuleName="AutoPatchDsc"; RequiredVersion="1.4.2"}
+
+    # AutoPatchDSC has a dependency on PSWindowsUpdate -- you will need to download this from the PowerShell Gallery website.
+    If (-not (Get-Module PSWindowsUpdate)) {Import-Module PSWindowsUpdate}
 
     Node 'Server01'{
         #Install patches during maintenance window
@@ -13,6 +15,7 @@ Configuration OnlyPatchServerNoReboot {
             PatchWindowStart              = 'January 1st, 2017 2am'
             PatchWindowEnd                = 'January 1st, 2017 4am'
             InstallPatchesDuringPreflight = $false
+            LogFile                       = 'c:\locallogs\AutoPatchInstall.log'
         }
     }
 }
